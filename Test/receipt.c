b899afc23287be888dd2d2c48584dd2bf2d8c0be
danasf9-dotcom <danasf9@gmail.com>
2026-04-29 17:27:03 +0000

    Test

---
+++ Test/receipt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    char item(60);
+    printf("item");
+    scanf("%d", &price);
+
+    float price;
+    printf("price ");
+    scanf("%d", &price);
+
+    int quality;
+    printf("quantity:");
+
+
+}
