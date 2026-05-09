7dd9e1a2d8c547bc223b7acf2c88ef44eeb868fb
danasf9-dotcom <danasf9@gmail.com>
2026-05-09 05:33:40 +0000

    terminado

--- temp2.c
+++ temp2.c
@@ -6,23 +6,23 @@ int main (void)
     printf("enter celcius");
     scanf("%d", &ce);
     // convert
-    int fa1 = (ce*1.8)+32
+    int fa1 = (ce*1.8)+32;
     printf("%d°C = %d°F \n", ce, fa1);
     // -40 fahrenheit= -40 celcius
     if(ce < 0){
-        prinf("❄️ Freezing weather \n");
+        printf("❄️ Freezing weather \n");
     }
     else if(ce < 10 ) {
         printf("🥶 Very cold weather \n");
     }
     else if(ce < 20 ) {
-        prinf("🧥 Chilly weather \n");
+        printf("🧥 Chilly weather \n");
     }
     else if(ce <30 ) {
         printf("🖼️ Normal weather \n");
     }
     else if(ce < 40 ) {
-        prinf("☀️ Hot weather \n");
+        printf("☀️ Hot weather \n");
     }
     else {
         printf("🔥 Very hot weather \n");
