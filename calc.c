baa2442ff4a887f15b1d85fe9d2a0a9649c85f9a
danasf9-dotcom <danasf9@gmail.com>
2026-05-13 03:53:54 +0000

    terminado(dana)

--- calc.c
+++ calc.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 //prototype
@@ -13,29 +14,31 @@ char ch;
     printf("Equation:\n");
     scanf("%f%c%f", &N1, &ch, &N2);
 
-    if (ch == ´+´){
+    if (ch == '+'){
         add();
     }
 
-    else if (ch == ´-´){
+    else if (ch =='-'){
         less();
     }
-    else if (ch == ´*´){
+    else if (ch =='*'){
         mult();
     }
-    else if (ch ==´/´){
+    else if (ch =='/'){
         divd();
     }
     else{
-        printf("Check spellin brothie\n");
+        printf("check spelling\n");
+    }
+
 
 }
 void add(void){
 
 //float N1;
 //float N2;
-  // printf("Equation:\n");
-    //scanf("%f+%f",&N1 , &N2);
+  //  printf("Equation:\n");
+    //scanf("%f+%f",&N1 ,&N2);
     float ans = N1 + N2;
     printf("%.2f\n",ans);
 }
@@ -44,7 +47,7 @@ void less(void){
 //float N1;
 //float N2;
 
-    //printf("Equation:\n");
+    // printf("Equation:\n");
      //scanf("%f-%f",&N1 ,&N2);
      float ans = N1 - N2;
      printf("%2.f\n",ans);
@@ -54,9 +57,9 @@ void mult(void){
 //float N1;
 //float N2;
 
-   //prinf("Equation:\n");
-   //scanf("%f*%f", &N1, &N2);
-     float ans = N1* N2;
+   // prinf("Equation:\n");
+   // scanf("%f*%f", &N1,&N2);
+     float ans = N1 * N2;
      printf("%.2f\n",ans);
 }
 void divd(void){
@@ -65,12 +68,12 @@ void divd(void){
 //float N2;
 
  // printf("Equation:\n");
-   //scanf("%f/%f", &N1 , &N2);
+   //scanf("%f/%f",&N1 ,&N2);
      float ans = N1 / N2;
              if (N2 == 0){
-                printf(" no can do brothie\n");
-             }
-             else{
-        printf("%.2f\n",ans);
-             }
+                printf("no can do\n");
+            }
+            else{
+    printf("%.2f\n",ans);
+            }
 }
